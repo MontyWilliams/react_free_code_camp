d9bd361d5df24efe6b06e2bc52f0f471da149563
Montywilliams <3650@holbertonschool.com>
2022-05-19 09:48:36 -0600

    gonna start using git pages so i can pull my app up on another cpu

---
+++ airBNB_Exp/src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Logo from "../images/airbnb-logo.png"
+
+
+export default function NavBar() {
+    return (
+        <div className="Rectangle13">
+            <img src={Logo} className="nav--icon" alt="logo" />
+        </div>
+    )
+}
